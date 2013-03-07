3bcf0053171854c204a14a195a072e12e51c4f12
Andrew Nguyen <atnguyen4@gmail.com>
2013-03-07 16:41:45 -0500

    Finished 11-20

--- Practice11_20.hs
+++ Practice11_20.hs
@@ -16,8 +16,34 @@ repli xs n = concatMap (\x -> replicate n x) xs
 
 -- #16
 dropEveryHelper [] _ _ = []
-dropEveryHelper (x:xs) n i =
-    if i == n 
-        then dropEveryHelper xs n 1
-        else x : dropEveryHelper xs n (i + 1)
+dropEveryHelper (x:xs) n i 
+    | i == n    = dropEveryHelper xs n 1
+    | otherwise = dropEveryHelper xs n (i + 1)
+    
 dropEvery xs n = dropEveryHelper xs n 1
+
+-- # 17
+splitHelper :: Int -> [a] -> [a] -> ([a], [a])
+splitHelper _ acc [] = (acc, [])
+splitHelper 0 acc l@(x:xs) = (acc, l)
+splitHelper n acc l@(x:xs) =  splitHelper (n - 1) (acc ++ [x]) xs 
+        
+split :: [a] -> Int -> ([a], [a])
+split xs n | n >= 0    = splitHelper n [] xs
+           | otherwise = splitHelper ((length xs) + n) [] xs 
+
+-- # 18
+slice :: [a] -> Int -> Int -> [a]
+slice xs a z = fst (split (snd (split xs (a - 1))) (z - a + 1)) 
+
+-- # 19
+rotate :: [a] -> Int -> [a]
+rotate xs n = zs ++ yz where (yz, zs) = split xs n
+     
+-- # 20
+removeAt :: Int -> [a] -> (a, [a])
+removeAt n (x:xs) | n == 1    = (x, xs)
+                  | otherwise = (e, x:ys) 
+                      where (e, ys) = removeAt (n - 1) xs
+
+
